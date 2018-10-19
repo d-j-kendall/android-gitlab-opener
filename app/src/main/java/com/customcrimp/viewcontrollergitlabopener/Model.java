301ffdbb951257dc830701902887e9e43b86c498
conti-tech <conti-tech@Michaels-MacBook-Pro.local>
2018-10-19 13:52:40 -0500

    Started on model

---
+++ app/src/main/java/com/customcrimp/viewcontrollergitlabopener/Model.java
@@ -0,0 +1,19 @@
+package com.customcrimp.viewcontrollergitlabopener;
+
+import android.widget.Button;
+import android.widget.RadioButton;
+
+public class Model {
+
+    String openTicketUrl = "https://gitlab.com/customcrimp/connected_controller/engineering_requests/issues/";
+    String newTicketUrl  = "https://gitlab.com/customcrimp/connected_controller/engineering_requests/issues/new?issue%5Bassignee_id%5D=&issue%5Bmilestone_id%5D=";
+    String emailTicketString = "incoming+customcrimp/connected_controller/engineering_requests+5n6u8yfgnxmmjy8ca84vs02ok@incoming.gitlab.com";
+    String mergeRequestUrl = "https://gitlab.com/customcrimp/connected_controller/plc-2017-connected_controller_programs/merge_requests/";
+    String searchTicketUrl  = "https://gitlab.com/customcrimp/connected_controller/engineering_requests/issues?scope=all&utf8=✓&state=opened&search=";
+    String wikiUrl = "https://gitlab.com/customcrimp/connected_controller/wiki-2017-general_documentation/blob/master/README.md";
+
+
+    openURL(RadioButton , Button)
+
+
+}
