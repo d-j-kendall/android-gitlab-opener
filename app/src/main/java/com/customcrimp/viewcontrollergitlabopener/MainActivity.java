928370bd503ed19bdc2efc3fade3930ffe5e082f
Dustin Kendall <dustin.kendall@customcrimp.com>
2018-10-26 11:46:07 -0500

    went to lunch almost finished

--- app/src/main/java/com/customcrimp/viewcontrollergitlabopener/MainActivity.java
+++ app/src/main/java/com/customcrimp/viewcontrollergitlabopener/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.RadioButton;
 import android.content.Intent;
 import android.net.Uri;
 import android.content.res.Resources;
+import android.widget.RadioGroup;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -19,10 +21,15 @@ public class MainActivity extends Activity {
     //private String emailTicketString = "incoming+customcrimp/connected_controller/engineering_requests+5n6u8yfgnxmmjy8ca84vs02ok@incoming.gitlab.com";
     private String mergeRequestUrl = "";
     private String searchTicketUrl  = "";
-    //private String wikiUrl = "https://gitlab.com/customcrimp/connected_controller/wiki-2017-general_documentation/blob/master/README.md";
+    private String wikiUrl = "https://gitlab.com/customcrimp/connected_controller/wiki-2017-general_documentation/blob/master/README.md";
     private boolean checked = false;
+    private boolean textBoxFirstClick = false;
     @BindView(R.id.editText)
     EditText editTextString;
+    @BindView(R.id.radioGroup1)
+    RadioGroup leftRadioGroup;
+    @BindView(R.id.radioGroup2)
+    RadioGroup rightRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,27 +40,33 @@ public class MainActivity extends Activity {
 
     public void onRadioSelect(View view){
         this.checked = ((RadioButton) view).isChecked();
+
         switch(view.getId()) {
             case R.id.engineering:
+                rightRadioGroup.clearCheck();
                 this.openTicketUrl = "https://gitlab.com/customcrimp/connected_controller/engineering_requests/issues/";
                 this.newTicketUrl = "https://gitlab.com/customcrimp/connected_controller/engineering_requests/issues/new?issue%5Bassignee_id%5D=&issue%5Bmilestone_id%5D=";
                 this.searchTicketUrl = "https://gitlab.com/customcrimp/connected_controller/engineering_requests/issues?scope=all&utf8=✓&state=opened&search=";
                 this.mergeRequestUrl = "https://gitlab.com/customcrimp/connected_controller/plc-2017-connected_controller_programs/merge_requests/";
+                this.wikiUrl
 
                 break;
             case R.id.crimpIq:
+                rightRadioGroup.clearCheck();
                 this.openTicketUrl = "https://gitlab.com/customcrimp/connected_controller/andr-2016-crimp_controller/issues/";
                 this.newTicketUrl = "https://gitlab.com/customcrimp/connected_controller/andr-2016-crimp_controller/issues/new?issue%5Bassignee_id%5D=&issue%5Bmilestone_id%5D=";
                 this.searchTicketUrl = "https://gitlab.com/customcrimp/connected_controller/andr-2016-crimp_controller/issues?scope=all&utf8=✓&state=opened&search=";
                 this.mergeRequestUrl = "https://gitlab.com/customcrimp/connected_controller/andr-2016-crimp_controller/merge_requests/";
                 break;
             case R.id.myCrimp:
+                leftRadioGroup.clearCheck();
                 this.openTicketUrl = "https://gitlab.com/customcrimp/connected_controller/issues-2018-mycrimp_issue_tracker/issues/";
                 this.newTicketUrl = "https://gitlab.com/customcrimp/connected_controller/issues-2018-mycrimp_issue_tracker/issues/new?issue%5Bassignee_id%5D=&issue%5Bmilestone_id%5D=";
                 this.searchTicketUrl = "https://gitlab.com/customcrimp/connected_controller/issues-2018-mycrimp_issue_tracker/issues?scope=all&utf8=✓&state=opened&search=";
                 this.mergeRequestUrl = "https://gitlab.com/customcrimp/mycrimp/andr-2016-mycrimp-all/merge_requests/";
                 break;
             case R.id.crimpCloud:
+                leftRadioGroup.clearCheck();
                 this.openTicketUrl = "https://gitlab.com/customcrimp/connected_controller/ruby-2016-crimp_cloud/issues/";
                 this.newTicketUrl = "https://gitlab.com/customcrimp/connected_controller/ruby-2016-crimp_cloud/issues/new?issue%5Bassignee_id%5D=&issue%5Bmilestone_id%5D=";
                 this.searchTicketUrl = "https://gitlab.com/customcrimp/connected_controller/ruby-2016-crimp_cloud/issues?scope=all&utf8=✓&state=opened&search=";
@@ -73,8 +86,8 @@ public class MainActivity extends Activity {
 
     public void onOpenTicket(View view){
         Log.d("log",editTextString.getText().toString());
-       // if(checked = true)
-         //   GoToURL(openTicketUrl);
+        if(checked = true)
+            GoToURL(openTicketUrl);
 
     }
     public void onNewTicket(View view){
@@ -93,9 +106,17 @@ public class MainActivity extends Activity {
     }
 
     private void GoToURL(String url){
-        Uri uri = Uri.parse(url);
+        Uri uri = Uri.parse(url+editTextString.getText().toString());
         Intent intent= new Intent(Intent.ACTION_VIEW,uri);
         startActivity(intent);
+    }
+    public void clearTextBox(View view){
+        if(!textBoxFirstClick){
+            editTextString.setText("");
+            textBoxFirstClick = true;
+        }
+
+
     }
 
 }
